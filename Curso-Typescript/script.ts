43949a2a815efe5c884c833dea54943bf7b959dd
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2023-09-26 20:45:13 -0300

    Excercise3 interfaces

--- Curso-Typescript/script.ts
+++ Curso-Typescript/script.ts
@@ -22,10 +22,47 @@ function totalMudou(){
 
 input?.addEventListener("keyup",totalMudou); */
 
-function toNumber(value : string | number){
+/* function toNumber(value : string | number){
     if(typeof value === "number") return value;
 
     if(typeof value === "string") return Number(value);
 
     throw "value deve ser number ou string";
+} */
+
+interface Empresa{
+        nome: string;
+        fundacao: number;
+        pais: string;
+}
+
+interface Produto {
+    nome: string;
+    preco: number;
+    descricao: string;
+    garantia: string;
+    seguroAcidentes: boolean;
+    empresaFabricante: Empresa
+    empresaMontadora: Empresa;
+  }
+
+
+async function fetchProduct() {
+    const response = await fetch("https://api.origamid.dev/json/notebook.json");
+    const data = await response.json();
+   showProduct(data);
+}
+
+fetchProduct();
+
+function showProduct(data:Produto){
+    document.body.innerHTML = `
+    <div>
+        <h2>${data.nome}</h2>
+        <p>${data.preco}</p>
+        <div>
+            <h3>Fabricante: ${data.empresaFabricante.nome}</h3>
+        </div>
+    </div>
+    `
 }
